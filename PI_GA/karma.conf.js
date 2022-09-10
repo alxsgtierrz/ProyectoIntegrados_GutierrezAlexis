40fd4314f681b7421559ee28f7abe337ce20137f
Alexis Gutierrez <alexisgutierrez1072@gmail.com>
2022-09-10 16:38:40 -0300

    Correcion de errores

--- PI_GA/karma.conf.js
+++ PI_GA/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pi-gutierrez-alexis'),
+      dir: require('path').join(__dirname, './coverage/pi-ga'),
       subdir: '.',
       reporters: [
         { type: 'html' },
