1407da1fc4775e8d6f4533d70f9a391d53c04a9d
Alexis Gutierrez <alexisgutierrez1072@gmail.com>
2022-09-13 21:15:41 -0300

    Redireccion

---
+++ PI_GA/BackEnd/src/main/java/com/portflio/ANG/AngApplication.java
@@ -0,0 +1,13 @@
+package com.portflio.ANG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngApplication.class, args);
+	}
+
+}
