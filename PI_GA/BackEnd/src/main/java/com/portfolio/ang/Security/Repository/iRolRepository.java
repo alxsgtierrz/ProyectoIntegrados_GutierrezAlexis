c8468ba6380e4c37788d5efefdc81f24ab6772d7
Alexis Gutierrez <alexisgutierrez1072@gmail.com>
2022-09-16 02:55:40 -0300

    BackEnd

---
+++ PI_GA/BackEnd/src/main/java/com/portfolio/ang/Security/Repository/iRolRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.ang.Security.Repository;
+
+import com.portfolio.ang.Security.Entity.Rol;
+import com.portfolio.ang.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer> {
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
